aa1cad5e7a6b43600acf789b72b3c427dad794ce
Alison Orton <orton.alison@gmail.com>
2022-10-18 17:06:55 -1000

    Update FunWithFunctions.java
    Displaying the domain and codomain

--- FunWithFunctions.java
+++ FunWithFunctions.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class FunWithFunctions {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Scanner to read in data
         var s = new Scanner(System.in);
         String input;
@@ -11,13 +11,44 @@ public class FunWithFunctions {
         String[] domain = input.split(" ");
         input = s.nextLine();
         String[] codomain = input.split(" ");
+        input = s.nextLine();
+        String[] relation = input.split(" ");
 
-        System.out.println(Arrays.toString(domain));
-        System.out.println(Arrays.toString(codomain));
+        //Turing the domain array into a string, so we can display it easily
+        displaySet(domain, 0);
+        displaySet(codomain, 1);
 
         //Logic to determine if it is a function, and if so what type
 
         //Displaying
+    }
+
+    public static void displaySet(String[] setInput, int setType){
+        //If setType = 0 it's the domain
+        //If setType = 1 it's the codomain
+
+        String displayOutput = "";
+        if(setType == 0){
+            displayOutput = "DOMAIN: { ";
+        }
+        else{
+            displayOutput = "CODOMAIN: { ";
+        }
+
+        for(int i = 0; i < setInput.length; i++){
+            if(i != setInput.length-1){
+                displayOutput += setInput[i] + ", ";
+            }
+            else{
+                displayOutput += setInput[i] + " }";
+            }
+        }
+
+        System.out.println(displayOutput);
+    }
+
+    public static void displayRelation(String[] relInput){
+        
     }
 
 }
